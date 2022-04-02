51c72513e7a9d17a9adaea42ba4f324b170b2d58
kezmorz <enterprise@mxrse.com>
2022-04-02 19:48:18 +0100

    Added initial about page structure.

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,60 @@
+import { useTranslations } from "use-intl";
+import { Container } from "@mui/material";
+import { loader as cloudinaryImageLoader } from "@/lib/cloudinary";
+import { pick } from "@/utils/misc";
+import Header from "@/components/Header";
+import Layout from "@/components/Layout";
+
 // about me
 // technical skills
 // about this website (how it was built)
 
 const About = () => {
-  return null;
+  const t = useTranslations("about");
+
+  return (
+    <>
+      <Header
+        heading={t("heading")}
+        subheading={t("subheading")}
+        imageProps={{
+          src: "samples/cloudinary-icon.png",
+          alt: "Something something something",
+          width: 480,
+          height: 350,
+          layout: "responsive",
+          loader: cloudinaryImageLoader,
+        }}
+        direction="ltr"
+      />
+      <Container
+        component="section"
+        maxWidth="md"
+        sx={{ mb: { xs: 8, sm: 16 } }}
+      ></Container>
+    </>
+  );
+};
+
+About.getLayout = (page) => {
+  return (
+    <Layout>
+      <main>{page}</main>
+    </Layout>
+  );
+};
+
+About.messages = ["about", ...Layout.messages];
+
+export const getStaticProps = async ({ locale }) => {
+  return {
+    props: {
+      messages: pick(
+        await import(`../translations/${locale}.json`),
+        About.messages
+      ),
+    },
+  };
 };
 
 export default About;
