8953d62d7b99c0b73a24d14063278e6a8f7e149c
kezmorz <enterprise@mxrse.com>
2022-05-22 18:55:14 +0100

    Modified fancy link component to apply active class name if path starts with current pathname.

--- src/components/Link/index.js
+++ src/components/Link/index.js
@@ -59,7 +59,10 @@ const Link = forwardRef(
     const router = useRouter();
     const pathname = typeof href === "string" ? href : href.pathname;
     const className = clsx(classNameProps, {
-      [activeClassName]: router.pathname === pathname && activeClassName,
+      [activeClassName]:
+        (router.pathname === pathname ||
+          router.pathname.startsWith(`${pathname}/`)) &&
+        activeClassName,
     });
 
     const isExternal =
