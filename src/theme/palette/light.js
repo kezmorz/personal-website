80a7966e4bf86eda2e94645f4ed11d42c1994b5f
kezmorz <enterprise@mxrse.com>
2022-08-03 23:59:54 +0100

    Testing light palette.

--- src/theme/palette/light.js
+++ src/theme/palette/light.js
@@ -1,5 +1,47 @@
+const teal = {
+  50: "#D6FFF5",
+  100: "#BFF7EA",
+  200: "#9DEDDA",
+  300: "#77DCCC",
+  400: "#52C4BA",
+  main: "#32A4A0",
+  500: "#21808B",
+  600: "#14586B",
+  700: "#104B65",
+  800: "#0D3D59",
+  900: "#092B3E",
+};
+
+const grey = {
+  50: "#FAFAFA",
+  100: "#EFEFEF",
+  200: "#DBDBDB",
+  300: "#BEBEBE",
+  400: "#AAAAAA",
+  500: "#9B9B9B",
+  600: "#767676",
+  700: "#575757",
+  800: "#424242",
+  900: "#1C1C1C",
+};
+
 const light = {
   mode: "light",
+  primary: {
+    ...teal,
+  },
+  grey: {
+    ...grey,
+  },
+  text: {
+    primary: grey[900],
+    secondary: grey[800],
+  },
+  divider: grey[900],
+  // background: {
+  //   default: grey[50],
+  //   paper: grey[100]
+  // }
 };
 
 export default light;
